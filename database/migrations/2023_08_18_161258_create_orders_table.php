8ba4db100ee8c1f144acf5b4b92868f37d5ab046
Hasitha <hasitha@efuturesworld.com>
2023-08-18 21:52:01 +0530

    controller and migrations

---
+++ database/migrations/2023_08_18_161258_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->double('value',8,2);
+            $table->date('date');
+            $table->integer('status');
+            $table->integer('process_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
